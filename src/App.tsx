bd05d19d5cd6ad73ca993f8cac93399af28bf7d7
Lucas Duarte <lucs.rkt@gmail.com>
2021-05-26 21:16:34 -0300

    adding path to malepage

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import MainPage from './MainPage';
 import StreetStyle from './StreetStyle';
 import NotFoundPage from './NotFoundPage';
 import MalePage from './MalePage'
+import Carousel from './components/Carousel'
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
@@ -11,12 +12,9 @@ function App() {
     <Router>
       <Switch>
         <Route path="/" component={LoginPage} exact></Route>
-<<<<<<< HEAD
         <Route path="/streetstyle" component={StreetStyle}></Route>
-=======
         <Route path="/MalePage" component={MalePage}></Route>
         <Route path="/carousel" component={Carousel}></Route>
->>>>>>> cf241d4d37d478226fd10e1de9f36a369e1c3a0f
         <Route path="/home" component={MainPage}></Route>
         <Route path="" component={NotFoundPage}></Route>
       </Switch>
