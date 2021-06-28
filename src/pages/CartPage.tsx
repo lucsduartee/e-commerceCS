bb00bfa4e43de7db9dce2193d4f5a92ea4a603ee
Lucas Duarte <lucs.rkt@gmail.com>
2021-06-27 23:47:46 -0300

    adding cartpage

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,16 +1,24 @@
 import styled from 'styled-components';
 import Header from '../components/Header';
+import React from 'react';
+import Footer from '../components/Footer';
+import CardBig from '../components/CardBig';
+
+import CartDescription from '../components/CartDescription';
 
 function CartPage(){
     return(
         <CartPageContainer>
             <Header />
+            <CartDescription />
+            <Footer />
         </CartPageContainer>
     )
 }
 
 const CartPageContainer = styled.div`
-    display: flex;
+   
+
 `;
 
 export default CartPage;
